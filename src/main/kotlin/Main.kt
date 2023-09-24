d6e32524456d9e74024bcbedbded73b5dab63654
Consultant <consultant@Admins-MBP-3.lan>
2023-09-24 16:55:28 -0400

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+    val animals = arrayOf(Hippo(), Wolf())
+    for (item in animals){
+        item.roam()
+        item.eat()
+    }
+
+    val vet = Vet()
+    val wolf = Wolf()
+    val hippo = Hippo()
+    vet.giveShot(wolf)
+    vet.giveShot(hippo)
+
+    val roamable = arrayOf(Hippo(), Wolf(), Vehicle())
+    for (item in roamable){
+        item.roam()
+        if (item is Animal){
+            item.eat()
+        }
+    }
+}
---
+++ src/main/kotlin/Vehicle.kt
@@ -0,0 +1,5 @@
+class Vehicle: Roamable {
+    override fun roam() {
+        println("The Vechicle is roaming")
+    }
+}
